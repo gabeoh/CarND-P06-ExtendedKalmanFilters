4cab56ecdae090e335a08879e30c17bfa61697bb
gabeoh <jungmoo.oh@gmail.com>
2018-09-23 12:54:02 -0400

    feat: Implement tools required for EKF
    - Tools::CalculateRMSE(): Implement Root Mean Square Error calculation
    - Tools::CalculateJacobian(): Implement Jacobian matrix calculation a given state vector

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,25 +1,62 @@
 #include <iostream>
+#include <stdexcept>
 #include "tools.h"
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
 
-Tools::Tools() {}
+Tools::Tools() = default;
 
-Tools::~Tools() {}
+Tools::~Tools() = default;
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  // Initialize RMSE to 0
+  rmse << 0, 0, 0, 0;
+
+  // Validate input vector sizes
+  size_t n = estimations.size();
+  if (n == 0 || ground_truth.size() != n) {
+    cout << "Invalid input vector sizes" << endl;
+    return rmse;
+  }
+
+  // Calculate the RMSE here.
+  // Accumulate residuals
+  for (int i = 0; i < n; ++i) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  // Calculate the mean
+  rmse /= n;
+
+  // Calculate the square root
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+  // Get state variables
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+  // Compute intermediate terms
+  float rho2 = px*px + py*py;
+  // Skip update if rho = 0 (prevent divide by 0)
+  if (rho2 == 0)
+    throw std::invalid_argument("Cannot compute Jacobian because rho = 0");
+  float rho = sqrt(rho2);
+  float rho3 = rho2 * rho;
+
+  MatrixXd Hj(3, 4);
+  Hj << px / rho, py / rho, 0, 0,
+        -py / rho2, px / rho2, 0, 0,
+        py*(vx*py - vy*px) / rho3, px*(vy*px - vx*py) / rho3, px/rho, py/rho;
+  return Hj;
 }
